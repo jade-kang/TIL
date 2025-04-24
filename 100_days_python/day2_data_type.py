b1146f8f463e22269bd51a77eaa60e60bd5f3611
parkc7788** <a01090314511@gmail.com>
2025-04-24 19:00:40 +0900

    day2_data_type

---
+++ 100_days_python/day2_data_type.py
@@ -0,0 +1,13 @@
+#print(int('123'))
+#문자열은 str, float 으로 변환 불가
+#---
+#이름을 입력받고, 이름의 길이를 반환하는 코드 작성하기
+# name_of_the_user = input('enter your name: ')
+# length_of_name = len(name_of_the_user) #문자열은 len 가능
+# print(type('number of letters in your name: ')) #str
+# print(type(length_of_name)) #int
+# #str + int 불가 !
+# print('number of letters in your name : ' + str(length_of_name))
+#---
+#print(5/3) #무조건 부동 소수점 동반 (부동소수점 없는 정수값 원하려면 //)
+#print(5//3) = 1
